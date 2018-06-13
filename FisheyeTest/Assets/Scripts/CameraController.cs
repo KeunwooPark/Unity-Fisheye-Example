b0c4508c1ff0af0a90fa0882e25a1d4791e43c21
keunwoo <avolic@kaist.ac.kr>
2018-06-13 15:45:40 +0900

    first commit

---
+++ FisheyeTest/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # unity_fisheye_test
+
+This is a sample code for make a fisheye camera in Unity.
+Followed sample code from http://paulbourke.net/dome/unityfisheye/
+
+You can move character with arrow keys.
+Fisheye camera view is rendered to Display 2.
